3ba4b30b02addfa74fc1912fc8c2fc64297eb55e
SmokerTobi <himeshbhure10@gmail.com>
2026-04-16 11:33:40 +0530

    Update WEEK_11.c

--- WEEK_11.c
+++ WEEK_11.c
@@ -1,80 +1,32 @@
-https://www.tinkercad.com/things/3sP7gnflnPc/editel
+https://www.tinkercad.com/things/eMaUQQVrUm2/editel
 
-#include <Servo.h>
-Servo servo;
+void setup()
+{ 
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+}
 
-int rled = 2;
-int gled = 3 ; 
-int roominus = 6;
-int roomoutus = 5;
-int buzzer = 13;
-int pb = 4;
-int buttonstate = 0;
-  
-void setup(){
-    Serial.begin(9600);
-	pinMode(buzzer, OUTPUT);
-    pinMode(rled, OUTPUT);
-    pinMode(gled, OUTPUT);
-    pinMode(pb,INPUT);
-    servo.attach(9);
-}  
+void loop()
+{
+  long duration,cm;
 
-void loop(){
-  digitalWrite(rled, LOW);
-  digitalWrite(gled, LOW);
-  servo.write(0);
+  pinMode(7, OUTPUT);
   
-  long disroomout = distancefromultrasonic(roomoutus);
-  long disroomin = distancefromultrasonic(roominus);
-  Serial.print("disroomout: ");
-  Serial.println(disroomout);
-  Serial.print("disroomin: ");
-  Serial.println(disroomin);
-  
-  if (disroomout < 50 && disroomin > 100){
-    digitalWrite(gled, HIGH);
-    for (int i = 0; i<=90; i=i+10)
-    	servo.write(i);
-    delay(500);
-	}
-  else if(disroomin < 50 && disroomout > 100){
-    digitalWrite(gled, HIGH);
-    for (int i = 0; i<=90; i=i+10)
-    	servo.write(i); 
-    delay(500);
+  digitalWrite(7, HIGH);
+  digitalWrite(7, LOW);
+
+  pinMode(7, INPUT);
+  duration = pulseIn(7, HIGH);
+  cm= duration/29/2;
+  if(cm<200)
+  {
+    digitalWrite(6,HIGH);
+    digitalWrite(5,LOW);
   }
-  else if(disroomin<100 && disroomout<50){
-    digitalWrite(rled, HIGH);
-    tone(buzzer,300,1000);
-    buttonstate = digitalRead(pb);
-    
-    Serial.println("Buttonstate: "); 
-    Serial.print(buttonstate);
-    if (buttonstate == HIGH)
-    {
-	  noTone(buzzer);
-      digitalWrite(gled, HIGH);
-      for (int i = 0; i<=90; i=i+15)
-    	servo.write(i);
-      delay(1000);
-    }
-  } delay(100);
-}   
-
-long distancefromultrasonic(int pinno){
-
-  pinMode(pinno, OUTPUT);
-  digitalWrite(pinno,LOW);
-  delay(0.002);
-  digitalWrite(pinno,HIGH);
-  delay(0.005);
-  digitalWrite(pinno,LOW);
+  if(cm>200)//reverse rotation
+  {
+    digitalWrite(5,HIGH);
+    digitalWrite(6,LOW);
+  }
   
-  pinMode(pinno, INPUT);
-  
-  long time = pulseIn(pinno,HIGH); 
-  long distance = time*0.0344/2;
-  return distance;
-
 }
